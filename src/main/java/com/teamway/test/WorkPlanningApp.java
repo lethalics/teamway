fddfeac1208f20fde7691fcce8cf26e98dc249ea
alex-gyg <alexandru.gheorghe-external@getyourguide.com>
2022-11-15 20:34:41 +0200

    Init spring boot app

---
+++ src/main/java/com/teamway/test/WorkPlanningApp.java
@@ -0,0 +1,11 @@
+package com.teamway.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPlanningApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkPlanningApp.class, args);
+    }
+}
